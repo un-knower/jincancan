db0c55c6a311c18245a2048a5233e771ff01ac40
张磊 <zhang_lei@boco.com.cn>
2017-12-21 14:02:20 +0800

    增加测试数据

--- brainhole-service/src/test/java/org/iplatform/microservices/brain/service/example/animals/AnimalsTest.java
+++ brainhole-service/src/test/java/org/iplatform/microservices/brain/service/example/animals/AnimalsTest.java
@@ -1,41 +1,12 @@
 package org.iplatform.microservices.brain.service.example.animals;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
-import org.datavec.api.records.reader.RecordReader;
-import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
-import org.datavec.api.split.FileSplit;
-import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
-import org.deeplearning4j.eval.Evaluation;
-import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
-import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.conf.layers.DenseLayer;
-import org.deeplearning4j.nn.conf.layers.OutputLayer;
-import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
-import org.deeplearning4j.nn.weights.WeightInit;
-import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.iplatform.microservices.brain.service.multiLayernetwork.FeedforwardNeuralNetworks;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.nd4j.linalg.activations.Activation;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.dataset.DataSet;
-import org.nd4j.linalg.dataset.SplitTestAndTrain;
-import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
-import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
-import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
-import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
 
 /**
  * 特征分类案例
@@ -62,13 +33,13 @@ public class AnimalsTest {
         FeedforwardNeuralNetworks feedforwardNeuralNetworks = new FeedforwardNeuralNetworks.FeedforwardNeuralNetworksBuilder(numInputs, outputNum).seed(6).iterations(1000).build();
         //训练模型
         boolean trainingSucceed = feedforwardNeuralNetworks.trainingCSV(new File("data/animals/animals_code.csv"), skipNumLines,
-                delimiter, percentTrain, batchSize, labelIndex, outputNum, numEpochs, hopeScore,false);
+                delimiter, percentTrain, batchSize, labelIndex, outputNum, numEpochs, hopeScore, false);
 
         //保存模型
         if (trainingSucceed) {
             String tempDir = System.getProperty("java.io.tmpdir");
             String modelDirectory = FilenameUtils.concat(tempDir, "brainhole/animals/");
-            feedforwardNeuralNetworks.saveModel(modelDirectory,"model.brain", Boolean.TRUE);
+            feedforwardNeuralNetworks.saveModel(modelDirectory, "model.brain", Boolean.TRUE);
 
             //使用模型进行分类预测
             int predictFileRowCount = 3;//预测数据3行
@@ -100,13 +71,13 @@ public class AnimalsTest {
         FeedforwardNeuralNetworks feedforwardNeuralNetworks = new FeedforwardNeuralNetworks.FeedforwardNeuralNetworksBuilder(numInputs, outputNum).seed(6).iterations(1000).build();
         //训练模型
         boolean trainingSucceed = feedforwardNeuralNetworks.trainingCSV(new File("data/animals/animals_code.csv"), skipNumLines,
-                delimiter, percentTrain, batchSize, labelIndex, outputNum, numEpochs, hopeScore,true);
+                delimiter, percentTrain, batchSize, labelIndex, outputNum, numEpochs, hopeScore, true);
 
         //保存模型
         if (trainingSucceed) {
             String tempDir = System.getProperty("java.io.tmpdir");
             String modelDirectory = FilenameUtils.concat(tempDir, "brainhole/animals/");
-            feedforwardNeuralNetworks.saveModel(modelDirectory,"model_earlystop.brain", Boolean.TRUE);
+            feedforwardNeuralNetworks.saveModel(modelDirectory, "model_earlystop.brain", Boolean.TRUE);
 
             //使用模型进行分类预测
             int predictFileRowCount = 3;//预测数据3行
